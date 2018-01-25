3a83c67e9a1c3454223f993abf65458d6ced1f2d
James Rochabrun <j@Jamess-MacBook-Pro-2.local>
2018-01-24 17:21:19 -0800

    code sample

--- TextViewResize/ViewController.swift
+++ TextViewResize/ViewController.swift
@@ -4,22 +4,76 @@
 //
 //  Created by James Rochabrun on 1/24/18.
 //  Copyright © 2018 James Rochabrun. All rights reserved.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    lazy var resizableTextView: UITextView = {
+        let tv = UITextView()
+        tv.translatesAutoresizingMaskIntoConstraints = false
+        tv.backgroundColor = UIColor.lightGray
+        tv.delegate = self
+        tv.font = UIFont.preferredFont(forTextStyle: .headline)
+        tv.isScrollEnabled = false
+        tv.text = "Default text just sto show \n again"
+        return tv
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setUpUI()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    private func setUpUI() {
+        view.addSubview(resizableTextView)
+        NSLayoutConstraint.activate([
+            resizableTextView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            resizableTextView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            resizableTextView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            resizableTextView.heightAnchor.constraint(equalToConstant: 50)
+            ])
+        /// calling the delegate
+        textViewDidChange(resizableTextView)
     }
-
-
 }
+
+extension ViewController: UITextViewDelegate {
+    
+    func textViewDidChange(_ textView: UITextView) {
+        let estimatedSize = textView.sizeThatFits(CGSize(width: view.frame.width, height: .infinity))
+        textView.constraints.forEach {
+            if $0.firstAttribute == .height {
+                $0.constant = estimatedSize.height
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
